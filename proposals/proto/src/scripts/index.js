d4aaf7d47550a7c347577d3a69453efd8e24d5f8
ciretorres <erictorres.velasco@gmail.com>
2025-03-27 14:24:26 -0600

    chore: anidando métodos y estructura

--- proposals/proto/src/scripts/index.js
+++ proposals/proto/src/scripts/index.js
@@ -12,12 +12,142 @@ let perspectiveCamera, controls, scene, renderer, stats;
 init();
 
 function init() {
+  //
+  // select canvas
+  const canvas = document.querySelector("#canvasid");
   // stats
   stats = new Stats();
   document.body.appendChild(stats.dom);
 
-  // select canvas
-  const canvas = document.querySelector("#canvasid");
+  function main() {
+    // Renders a view that contains your camera's "picture"
+    renderer = createWebGLRenderer(canvas);
+    renderer.setPixelRatio(window.devicePixelRatio);
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    // renderer.setAnimationLoop(animate);
+    // document.body.appendChild(renderer.domElement);
+
+    if (WebGL.isWebGL2Available()) {
+      // Initiate function or other initializations here
+      renderer.setAnimationLoop(animate);
+      console.log(WebGL.isWebGL2Available());
+    } else {
+      // Mostrar mensaje de Advertencia WebGL no compatible
+      canvas.style.display = "none";
+      const warning = WebGL.getWebGL2ErrorMessage();
+      document.querySelector("main").appendChild(warning);
+      const AdvertenciaWebGLNoCompatible = document.createElement("div");
+      AdvertenciaWebGLNoCompatible.innerHTML += `
+        <h2>
+          Tu tarjeta gráfica parece no soportar
+          <a
+            href="http://khronos.org/webgl/wiki/Getting_a_WebGL_Implementation"
+            target="_blank"
+            rel="noopener noreferrer">
+            WebGL 2
+          </a>
+        </h2>`;
+      document
+        .querySelector("#webglmessage")
+        .appendChild(AdvertenciaWebGLNoCompatible);
+    }
+
+    // universe
+
+    // A scene is the space in which you can places objects,cameras and lighting
+    scene = createScene(new THREE.Color(0x000000));
+    // scene.fog = new THREE.FogExp2(0xcccccc, 0.002);
+
+    // Adds a camera
+    // A perspective view that simulates the behaviour of a film camera in real life
+    // const aspect = window.innerWidth / window.innerHeight;
+    // perspectiveCamera = createPerspectiveCamera(60, aspect, 0.1);
+    perspectiveCamera = createPerspectiveCamera(60);
+
+    const cameraPositionZ = (width) => {
+      if (window.innerWidth <= width) {
+        return 80;
+      } else {
+        return 50;
+      }
+    };
+    perspectiveCamera.position.z = cameraPositionZ(375);
+
+    // Creates a box, geometry, 3d model, cube or mesh
+    const radius = 24;
+    // const geometry = createSphereGeometry(1, 5, 3);
+    const geometry = createSphereGeometry(radius, 32, 32);
+
+    // Creates a material that describe the appereance of objects
+    const material = createMeshBasicMaterial(new THREE.Color("#7833aa"));
+
+    // Adds the geometry to the mesh and apply the material to it
+    const mesh = createMesh(geometry, material);
+    mesh.updateMatrix();
+    // // mesh.matrixAutoUpdate = false;
+    scene.add(mesh);
+
+    // TODO: lights
+    // const dirLight1 = getLight();
+    // dirLight1.position.set(1, 1, 1);
+    // const dirLight2 = getLight(0x002288);
+    // dirLight2.position.set(-1, -1, -1);
+    // const ambientLight = new THREE.AmbientLight(0x555555);
+    // scene.add(ambientLight);
+
+    // TODO: HUD/GUI
+    // const gui = new GUI();
+
+    //
+
+    window.addEventListener("resize", onWindowResize);
+
+    createTrackballControls(perspectiveCamera);
+
+    function createTrackballControls(camera) {
+      controls = new TrackballControls(camera, renderer.domElement);
+      controls.rotateSpeed = 1.0;
+      controls.zoomSpeed = 1.2;
+      controls.panSpeed = 0.8;
+      controls.keys = ["KeyA", "KeyS", "KeyD"];
+    }
+
+    function onWindowResize() {
+      let SCREEN_HEIGHT = window.innerHeight;
+      let SCREEN_WIDTH = window.innerWidth;
+      const aspect = SCREEN_WIDTH / SCREEN_HEIGHT;
+
+      perspectiveCamera.aspect = aspect;
+      perspectiveCamera.updateProjectionMatrix();
+
+      renderer.setSize(SCREEN_WIDTH, SCREEN_HEIGHT);
+
+      controls.handleResize();
+    }
+
+    function animate() {
+      controls.update();
+
+      render();
+
+      stats.update();
+    }
+
+    function rotarMesh() {
+      // mesh.rotation.x += 0.001;
+      // mesh.rotation.y += 0.001;
+      mesh.rotation.x += 0.0001;
+      mesh.rotation.y += 0.0001;
+    }
+
+    function render() {
+      const camera = perspectiveCamera;
+
+      rotarMesh();
+
+      renderer.render(scene, camera);
+    }
+  }
 
   /**
    * Renders a view that contains your camera's "picture"
@@ -31,39 +161,6 @@ function init() {
       canvas,
     });
   };
-  renderer = createWebGLRenderer(canvas);
-  renderer.setPixelRatio(window.devicePixelRatio);
-  renderer.setSize(window.innerWidth, window.innerHeight);
-  // renderer.setAnimationLoop(animate);
-  // document.body.appendChild(renderer.domElement);
-
-  if (WebGL.isWebGL2Available()) {
-    // Initiate function or other initializations here
-    renderer.setAnimationLoop(animate);
-    console.log(WebGL.isWebGL2Available());
-  } else {
-    // Mostrar mensaje de Advertencia WebGL no compatible
-    canvas.style.display = "none";
-    const warning = WebGL.getWebGL2ErrorMessage();
-    document.querySelector("main").appendChild(warning);
-    const AdvertenciaWebGLNoCompatible = document.createElement("div");
-    AdvertenciaWebGLNoCompatible.innerHTML += `
-        <h2>
-          Tu tarjeta gráfica no soporta
-          <a
-            href="http://khronos.org/webgl/wiki/Getting_a_WebGL_Implementation"
-            target="_blank"
-            rel="noopener noreferrer">
-            WebGL 2
-          </a>
-        </h2>`;
-    document
-      .querySelector("#webglmessage")
-      .appendChild(AdvertenciaWebGLNoCompatible);
-  }
-
-  // universe
-
   /**
    * a scene is the space in which you can places objects,cameras and lighting
    * @see https://threejs.org/docs/#api/en/scenes/Scene
@@ -73,9 +170,6 @@ function init() {
     scene.background = backgroundColor;
     return scene;
   };
-  scene = createScene(new THREE.Color(0x000000));
-  // scene.fog = new THREE.FogExp2(0xcccccc, 0.002);
-
   /**
    * Adds a camera
    * A perspective view that simulates the behaviour of a film camera in real life
@@ -95,20 +189,6 @@ function init() {
     perspectiveCamera = new THREE.PerspectiveCamera(fov, aspect, near, far);
     return perspectiveCamera;
   };
-  // const aspect = window.innerWidth / window.innerHeight;
-  // perspectiveCamera = createPerspectiveCamera(60, aspect, 0.1);
-  perspectiveCamera = createPerspectiveCamera(60);
-
-  const cameraPositionZ = (width) => {
-    if (window.innerWidth <= width) {
-      return 80;
-    } else {
-      return 50;
-    }
-  };
-  perspectiveCamera.position.z = cameraPositionZ(375);
-
-  // Creates a box, geometry, 3d model, cube or mesh
   /**
    * Creates a geometry
    * @property {radius}:
@@ -143,9 +223,6 @@ function init() {
     );
     return geometry;
   };
-  const radius = 24;
-  const geometry = createSphereGeometry(radius, 32, 32);
-
   /**
    * Creates a material that describe the appereance of objects
    * @property {color}:
@@ -164,8 +241,6 @@ function init() {
     });
     return material;
   };
-  const material = createMeshBasicMaterial(new THREE.Color("#7833aa"));
-
   /**
    * Adds the geometry to the mesh and apply the material to it
    * @property {geometry}:
@@ -176,11 +251,6 @@ function init() {
     // scene.add(mesh);
     return mesh;
   };
-  const mesh = createMesh(geometry, material);
-  mesh.updateMatrix();
-  // // mesh.matrixAutoUpdate = false;
-  scene.add(mesh);
-
   // lights
   // const getLight = (color = 0xffffff, intensity = 3) => {
   //   let light = new THREE.DirectionalLight(color, intensity);
@@ -188,61 +258,5 @@ function init() {
   //   scene.add(light);
   //   return light;
   // };
-  // const dirLight1 = getLight();
-  // dirLight1.position.set(1, 1, 1);
-  // const dirLight2 = getLight(0x002288);
-  // dirLight2.position.set(-1, -1, -1);
-  // const ambientLight = new THREE.AmbientLight(0x555555);
-  // scene.add(ambientLight);
-
-  // TODO: HUD/GUI
-  // const gui = new GUI();
-
-  //
-
-  window.addEventListener("resize", onWindowResize);
-
-  createTrackballControls(perspectiveCamera);
-
-  function createTrackballControls(camera) {
-    controls = new TrackballControls(camera, renderer.domElement);
-    controls.rotateSpeed = 1.0;
-    controls.zoomSpeed = 1.2;
-    controls.panSpeed = 0.8;
-    controls.keys = ["KeyA", "KeyS", "KeyD"];
-  }
-
-  function onWindowResize() {
-    let SCREEN_HEIGHT = window.innerHeight;
-    let SCREEN_WIDTH = window.innerWidth;
-    const aspect = SCREEN_WIDTH / SCREEN_HEIGHT;
-
-    perspectiveCamera.aspect = aspect;
-    perspectiveCamera.updateProjectionMatrix();
-
-    renderer.setSize(SCREEN_WIDTH, SCREEN_HEIGHT);
-
-    controls.handleResize();
-  }
-
-  function animate() {
-    controls.update();
-
-    render();
-
-    stats.update();
-  }
-
-  function rotarMesh() {
-    mesh.rotation.x += 0.001;
-    mesh.rotation.y += 0.001;
-  }
-
-  function render() {
-    const camera = perspectiveCamera;
-
-    rotarMesh();
-
-    renderer.render(scene, camera);
-  }
+  main();
 }
