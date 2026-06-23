bf678b3705f2ffe590e5b37b6a9e93180b9e6e91
ciretorres <erictorres.velasco@gmail.com>
2026-06-22 19:56:38 -0600

    feat: agregando culling personalizado para sprites de estrellas

--- src/script.js
+++ src/script.js
@@ -1,11 +1,12 @@
 // import * as THREE from "three";
-import { AxesHelper, Group, Mesh } from "three";
+import { AxesHelper, Frustum, Group, Matrix4, Mesh } from "three";
 
 // componentes
 import camara from "./components/camara";
 import newControls from "./components/controls";
 import escena from "./components/escena";
 import newLights from "./components/lights";
+import { starsSprite } from "./components/models/estrellas";
 import implementacion from "./components/models/index.js";
 import newRenderer from "./components/renderer";
 import newStats from "./components/stats";
@@ -96,13 +97,105 @@ function init() {
 
     // Mesh para integrar a scene
 
+    // Raycaster para detección de clics
+    // let selectedObject = null;
+    // const raycaster = new Raycaster();
+    // const mouse = new Vector2();
+
+    // const onMouseClick = (event) => {
+    //   // console.log(event);
+    //   const mouseX = (event.clientX / window.innerWidth) * 2 - 1;
+    //   const mouseY = -(event.clientY / window.innerHeight) * 2 + 1;
+
+    //   // Actualiza el raycaster
+    //   raycaster.setFromCamera(mouse, camera);
+    //   console.log(group.children[0]);
+    //   // Intersecta con los objetos en la escena
+    //   const intersects = raycaster.intersectObjects(group, true);
+    //   console.log(intersects[0]);
+
+    //   if (intersects.length > 0) {
+    //     const selectedObject = intersects[0].object;
+    //     console.log(selectedObject);
+
+    //     // // Obtener información del objeto seleccionado
+    //     // console.log('Objeto seleccionado:', selectedObject);
+    //     // console.log('Posición actual:', selectedObject.position);
+
+    //     // // Modificar la distancia del objeto (ejemplo: alejarlo)
+    //     // selectedObject.position.z += 1; // Aumenta la distancia en el eje Z
+    //   }
+    // };
+
+    // // Detección de clics en sprites
+    // document.addEventListener("pointermove", onMouseClick);
+    // renderer.domElement.addEventListener("click", (event) => {
+    //   const mouseX = (event.clientX / window.innerWidth) * 2 - 1;
+    //   const mouseY = -(event.clientY / window.innerHeight) * 2 + 1;
+    //   // Actualizamos las coordenadas del mouse en el espacio de la cámara
+    //   camera.updateMatrixWorld(); // make sure the camera's matrix is updated
+    //   const vector = new Vector3(mouseX, mouseY, 0.5).unproject(camera);
+
+    //   // Creamos un raycaster para detectar qué sprite fue clickeado
+    //   const raycaster = new Raycaster();
+    //   raycaster.setFromCamera(vector.sub(camera.position), camera);
+    //   console.log(starsSprite[0].sprite);
+    //   // const intersects = raycaster.intersectObjects(
+    //   //   starsSprite.map((sprite) => sprite.sprite.isObject3D),
+    //   // ); // Ajuste necesario porque los sprites no son objetos 3D estándar
+    //   // console.log(intersects);
+    //   // if (intersects.length > 0) {
+    //   //   console.log("Sprite seleccionado:", intersects[0].object.userData); // Suponiendo que hay información adicional en userData
+    //   //   // Ejemplo: Cambiar la distancia del sprite seleccionado a 10 unidades desde el centro
+    //   //   // changeDistance(intersects[0].object, 10);
+    //   // }
+    // });
+
     //--
+
+    const updateCullingStarsVisibility = (camera) => {
+      // Culling personalizado
+      // console.log(starsSprite.filter((sprite) => sprite.sprite.visible).length);
+
+      // Actualizar la matriz de la cámara para asegurar que los cálculos sean correctos
+      camera.updateMatrixWorld();
+
+      // Obtener objetos visibles en el frustum
+      const frustum = new Frustum();
+
+      // Obtener la matriz de corte (frustum) a partir de la perspectiva actual de la cámara
+      frustum.setFromProjectionMatrix(
+        new Matrix4().multiplyMatrices(
+          camera.projectionMatrix,
+          camera.matrixWorldInverse,
+        ),
+      );
+
+      starsSprite.forEach((sprite, idx) => {
+        const star = sprite.sprite;
+        const isVisible = frustum.containsPoint(star.position);
+
+        // Verificar si la estrella está dentro del frustum de la cámara
+        if (isVisible) {
+          star.visible = isVisible; // Establece visible o no según el resultado
+          scene.add(star);
+        } else {
+          star.visible = isVisible; // Establece visible o no según el resultado
+          scene.remove(star);
+        }
+      });
+    };
 
     // render
     function render() {
       requestAnimationFrame(render);
       // group.rotation.x += 0.005;
       // group.rotation.y += 0.005;
+
+      // rotarMesh(group);
+
+      updateCullingStarsVisibility(camera);
+
       renderer.render(scene, camera);
       // window.requestAnimationFrame(animate);
 
@@ -123,8 +216,6 @@ function init() {
       // time *= 0.001; // convert time to seconds
       // controls.update();
       orbit.update();
-
-      // rotarMesh(group);
 
       render();
 
