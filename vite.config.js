1d9a5025c1ee27bb2e496ae44c6c9da0daf9b217
ciretorres <erictorres.velasco@gmail.com>
2026-06-24 21:39:06 -0600

    test: vite config plugin react y vue

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,24 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+// import vue from '@vitejs/plugin-vue'
+
 const isCodeSandbox =
   "SANDBOX_URL" in process.env || "CODESANDBOX_HOST" in process.env;
 
 export default {
+  // config options
+  // plugins: [react()],
+  // plugins: [vue()],
   root: "src/",
   publicDir: "../static/",
   base: "./",
+  resolve: {
+    alias: {
+      // "@": "/src",
+    },
+  },
   server: {
+    port: 3000,
     host: true,
     // open: !isCodeSandbox, // Open if it's not a CodeSandbox
   },
