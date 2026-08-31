67f8affc9ec331d6201f937e9ee3eef38458cc7a
ciretorres <erictorres.velasco@gmail.com>
2026-08-31 00:19:41 -0600

    fix: había un error en el fetch y caché

--- src/scripts/utils/fetch.js
+++ src/scripts/utils/fetch.js
@@ -1,6 +1,6 @@
 async function fetchData() {
   let data = [];
-  const url = "../notebooks/data_clean/vizier_I_239_hip_main_10pc.json";
+  const url = "../../notebooks/data_clean/vizier_I_239_hip_main_10pc.json";
 
   try {
     const response = await fetch(url);
