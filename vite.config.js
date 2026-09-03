9ceb09879e874da963c7e7fac805b1b84f50c7ec
ciretorres <erictorres.velasco@gmail.com>
2026-09-03 16:02:23 -0600

    feat: agregando vite config js

---
+++ vite.config.js
@@ -0,0 +1,34 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+// import vue from '@vitejs/plugin-vue'
+
+const isCodeSandbox =
+  "SANDBOX_URL" in process.env || "CODESANDBOX_HOST" in process.env;
+
+export default {
+  // config options
+  // plugins: [react()],
+  // plugins: [vue()],
+  // root: "src/",
+  root: "./",
+  // publicDir: "../static/",
+  publicDir: "../public/",
+  // base: "./",
+  base: "/thesis-project/",
+  resolve: {
+    alias: {
+      // "@": "/src",
+    },
+  },
+  server: {
+    // port: 3000,
+    host: true,
+    // open: !isCodeSandbox, // Open if it's not a CodeSandbox
+  },
+  build: {
+    outDir: "../dist",
+    emptyOutDir: true,
+    sourcemap: true,
+    // target: ["es2022", "chrome97", "edge97", "firefox96", "safari15"],
+  },
+};
